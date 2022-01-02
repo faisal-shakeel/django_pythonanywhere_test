5eccca3cbd3d1e3f3105c71e07ee6df54512aa78
faisal-shakeel <57106680+faisal-shakeel@users.noreply.github.com>
2022-01-03 01:32:03 +0500

    Removed rest framework

--- home/views.py
+++ home/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 # Create your views here.
 
-@api_view(['GET'])
 def home(request):
     return render(request,"home/home.html")
     
